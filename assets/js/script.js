06844029eb8c6d8ee29099545eb1ca1d77f08407
MariekeDijsselhof <MariekeDijsselhof@users.noreply.github.com>
2023-02-23 10:52:31 +0100

    howdy fellas

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     // functions go here
 
     var coll = document.getElementsByClassName("collapsible");
+    var menu = document.getElementById("menu");
     var i;
 
     for (i = 0; i < coll.length; i++) {
@@ -16,18 +17,32 @@ $(document).ready(function () {
             }
         });
     }
+
+    for (i = 0; i < menu.length; i++) {
+        menu[i].addEventListener("click", function () {
+            this.getElementById.toggle("active");
+            var content = this.firstElementChild;
+            if (content.style.display === "block") {
+            } else {
+                content.style.display = "none";
+            }
+        });
+    }
+
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function () {
         var currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) {
             document.getElementById("menu").style.top = "-6.0vh";
         } else {
-            document.getElementById("menu").style.top = "0vh";
+            document.getElementById("menu").style.top = "5.2vh";
         }
         prevScrollpos = currentScrollPos;
     }
 
 });
+
+
 
 function show() {
 
@@ -49,4 +64,36 @@ new Vue({
     computed: {},
     methods: {}
 });
+
+var vid = document.getElementById("myVideo");
+
+function enableMute() {
+    vid.muted = true;
+}
+
+window.addEventListener('load', videoScroll);
+window.addEventListener('scroll', videoScroll);
+
+function videoScroll() {
+
+    if (document.querySelectorAll('video[autoplay]').length > 0) {
+        var windowHeight = window.innerHeight,
+            videoEl = document.querySelectorAll('video[autoplay]');
+
+        for (var i = 0; i < videoEl.length; i++) {
+
+            var thisVideoEl = videoEl[i],
+                videoHeight = thisVideoEl.clientHeight,
+                videoClientRect = thisVideoEl.getBoundingClientRect().top;
+
+            if (videoClientRect <= ((windowHeight) - (videoHeight * .5)) && videoClientRect >= (0 - (videoHeight * .5))) {
+                thisVideoEl.play();
+            } else {
+                thisVideoEl.pause();
+            }
+
+        }
+    }
+
+}
 
